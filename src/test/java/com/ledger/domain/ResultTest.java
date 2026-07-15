6ef31d4f381bd9617387aa3797e9041838b484c2
Avinaash Ghansam <avinaash.ghansam.1@gmail.com>
2026-07-14 23:34:51 -0400

    finished Result

--- src/test/java/com/ledger/domain/ResultTest.java
+++ src/test/java/com/ledger/domain/ResultTest.java
@@ -17,4 +17,33 @@ class ResultTest {
         .extracting(r -> ((Result.OK<?, ?>) r).value())
         .isEqualTo(20);
   }
+
+  @Test
+  void map_onErr_doesNotRunFunction() {
+    // Arrange
+    Result<Integer, String> result = Result.err("FAILED");
+    // Act
+    Result<Integer, String> mapped =
+        result =
+            result.map(
+                val -> {
+                  throw new RuntimeException("This should not run");
+                });
+    // Assert
+    assertThat(mapped).isInstanceOf(Result.Err.class);
+    Result.Err<?, ?> errBox = (Result.Err<?, ?>) mapped;
+    assertThat(errBox.error()).isEqualTo("FAILED");
+  }
+
+  @Test
+  void flatMap_returnsFlattenedResult() {
+    // Arrange
+    Result<Integer, String> result = Result.ok(10);
+    // Act
+    Result<Integer, String> mapped = result.flatMap(val -> Result.ok(val * 2));
+    // Assert
+    assertThat(mapped).isInstanceOf(Result.OK.class);
+    Result.OK<?, ?> okBox = (Result.OK<?, ?>) mapped;
+    assertThat(okBox.value()).isEqualTo(20);
+  }
 }
